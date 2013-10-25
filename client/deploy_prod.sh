92f8e1cdf0d80426d681b426508321a30642b5f9
Maksim Solovjov <himaxx2@gmail.com>
2013-10-25 02:52:04 +0100

    Create a deployment mechanism
    - introducing two stages, beta and prod
    - both are actually identical, but don't deploy to prod until you
      are sure, that your version is working
    - always run deploy scripts from their folder
    - client/deploy* simply copies files into appropriate locations
    - server/deploy* compiles the code, runs tests and then copies the
      executable to the right place
    - IMPORTANT! you might want to edit local versions of server deploy
      because I had to reexport variables there, if your GOPATH is
      different.
    - As a side note, there should be specific folder structure for GO,
      I will expand on wiki sometime soon.

---
+++ client/deploy_prod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+# moving static files
+cp -r ./css ../beta/static/
+cp -r ./js ../beta/static/
+cp -r ./lib ../beta/static/
+
+# moving index file to templates
+cp ./index.html ../beta/static/templates/
