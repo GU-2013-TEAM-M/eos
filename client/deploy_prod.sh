f05e41e0812e000107c18cfc9a71e2292a2cf25a
Maksim Solovjov <himaxx2@gmail.com>
2013-10-25 02:58:50 +0100

    Fix client deploy scripts
    They we doing the opposite, beta deployed to prod, prod to beta...

--- client/deploy_prod.sh
+++ client/deploy_prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 # moving static files
-cp -r ./css ../beta/static/
-cp -r ./js ../beta/static/
-cp -r ./lib ../beta/static/
+cp -r ./css ../prod/static/
+cp -r ./js ../prod/static/
+cp -r ./lib ../prod/static/
 
 # moving index file to templates
-cp ./index.html ../beta/static/templates/
+cp ./index.html ../prod/static/templates/
