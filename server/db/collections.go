29bfe08b150e0ee0fe75839a15bffbd2e204c329
Maksim Solovjov <himaxx2@gmail.com>
2013-11-27 21:29:32 +0000

    Create the structures used to retrieve database records

--- server/db/collections.go
+++ server/db/collections.go
@@ -7,7 +7,34 @@ type Message struct {
     Msg string
 }
 
+type Organisation struct {
+    Id bson.ObjectId `bson:"_id"`
+    Name string
+
 type User struct {
+    Id bson.ObjectId `bson:"_id"`
+    OrgId bson.ObjectId
     Email string
     Password string
+}
+
+type Daemon struct {
+    Id bson.ObjectId `bson:"_id"`
+    OrgId bson.ObjectId
+    Name string
+    Status int
+}
+
+type Data struct {
+    Id bson.ObjectId `bson:"_id"`
+    Type string
+    Period string
+    Values []float64
+}
+
+type Report struct {
+    Id bson.ObjectId `bson:"_id"`
+    Type string
+    Period string
+    Values []float64
 }
