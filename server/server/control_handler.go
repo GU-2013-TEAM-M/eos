623a65770c032dd691ae86e173ce8e02661ba8dd
Maksim Solovjov <himaxx2@gmail.com>
2013-12-09 02:11:47 +0000

    Pipe the control request to daemon, initial version

--- server/server/control_handler.go
+++ server/server/control_handler.go
@@ -7,5 +7,20 @@ import (
 
 // a handler that checks that the current session id is still active
 func ControlHandler(cmd *CmdMessage) error {
-    return errors.New("Not implemented")
+    //sessId := cmd.Data["session_id"].(string)
+    daemonId := cmd.Data["daemon_id"].(string)
+
+    data := make(map[string]interface{})
+
+    daemon, ok := cmd.Conn.owner.(*User).GetOrg().Daemons[daemonId]
+    if !ok {
+        data["status"] = "NOT_OK"
+        DispatchMessage("control", data, cmd.Conn)
+        return errors.New("Trying to control inexistent daemon")
+    }
+
+    data["status"] = "OK"
+    DispatchMessage("control", data, cmd.Conn)
+    DispatchMessage("control", cmd.Data, daemon)
+    return nil
 }
