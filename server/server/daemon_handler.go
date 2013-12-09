d751f9782a21eaa8066d4fec0e3b3d254fce3576
Maksim Solovjov <himaxx2@gmail.com>
2013-12-09 06:21:30 +0000

    Add type checks for daemon handler
    Checking that id is string

--- server/server/daemon_handler.go
+++ server/server/daemon_handler.go
@@ -8,7 +8,10 @@ import (
 // a handler that provides the detailed information about the daemon
 func DaemonHandler(cmd *CmdMessage) error {
     //sessId := cmd.Data["session_id"].(string)
-    daemonId := cmd.Data["daemon_id"].(string)
+    daemonId, ok := cmd.Data["daemon_id"].(string)
+    if !ok {
+        return errors.New("wrong daemon_id supplied")
+    }
 
     data := make(map[string]interface{})
 
