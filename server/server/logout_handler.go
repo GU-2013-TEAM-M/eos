63c2b3b119592ec1af38cdc7259a0c6eb0793794
Maksim Solovjov <himaxx2@gmail.com>
2014-01-19 20:19:41 +0000

    Add tests and actual implementation for the logout handler

--- server/server/logout_handler.go
+++ server/server/logout_handler.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+    "eos/server/db"
+    "labix.org/v2/mgo/bson"
+)
+
 // a handler that deauthorises the user or the daemon
 func LogoutHandler(cmd *CmdMessage) error {
     data := make(map[string]interface{})
     cmd.Conn.owner.Deauthorise()
+
+    // if it is user, delete the session
+    if cmd.Conn.owner.IsUser() {
+        db.C("sessions").RemoveAll(bson.M{
+            "uid": bson.ObjectIdHex(cmd.Conn.owner.(*User).Id),
+        })
+    }
+
     data["status"] = "OK"
     DispatchMessage("logout", data, cmd.Conn)
     return nil
---
+++ server/server/logout_handler_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "testing"
+    "eos/server/test"
+    "eos/server/db"
+    "labix.org/v2/mgo/bson"
+)
+
+func Test_LogoutHandler(t *testing.T) {
+    // a user is removed from the organisation
+    // and his session is deleted
+    tmpS := &db.Session{UId: bson.ObjectIdHex("52a4ed348350a921bd000001")}
+    db.AddTemp("sessions", tmpS)
+
+    user := &User{
+        Id: "52a4ed348350a921bd000001",
+        SessionId: tmpS.GetId().Hex(),
+        OrgId: "Anonymous",
+    }
+    lcmd := &CmdMessage{
+        Data: make(map[string]interface{}),
+        Conn: &Connection{owner: user},
+    }
+    user.Authorise()
+
+    test.Assert(user.IsAuthorised(), "user is authorised before logout", t)
+
+    err := LogoutHandler(lcmd)
+
+    test.Assert(err == nil, "it logs out successfully", t)
+    test.Assert(!user.IsAuthorised(), "user is not in the organisation any more", t)
+    err = db.C("sessions").Find(bson.M{
+        "uid": bson.ObjectIdHex("52a4ed348350a921bd000001"),
+    }).One(tmpS)
+    test.Assert(err != nil, "there are no sessions for this user", t)
+
+    // it does not break if session was already gone
+    err = LogoutHandler(lcmd)
+    test.Assert(err == nil, "it does not blow up without session", t)
+
+    // a daemon is removed from the organisation
+    daemon := &Daemon{
+        Id: "52a4ed348350a921bd000001",
+        OrgId: "Anonymous",
+    }
+    lcmd = &CmdMessage{
+        Data: make(map[string]interface{}),
+        Conn: &Connection{owner: daemon},
+    }
+    daemon.Authorise()
+
+    test.Assert(daemon.IsAuthorised(), "daemon is authorised before logout", t)
+
+    err = LogoutHandler(lcmd)
+
+    test.Assert(err == nil, "it logs out successfully", t)
+    test.Assert(!daemon.IsAuthorised(), "daemon is not in the organisation any more", t)
+}
+
