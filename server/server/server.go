97b773ef7efc320692aa82bd24a669b5fc870b84
Maksim Solovjov <himaxx2@gmail.com>
2014-01-29 01:38:21 +0000

    Add dummy users for now
    email: a@a.ua or b@a.ua
    pass: hardpass

--- server/server/server.go
+++ server/server/server.go
@@ -23,6 +23,9 @@ func main() {
     RegisterAllHandlers()
     go h.run()
 
+    // create the dummy users for now
+    createDummyUsers()
+
     http.HandleFunc("/", rootHandler)
     http.HandleFunc("/client", chatHandler)
     http.Handle("/static/", http.FileServer(http.Dir("")))
@@ -32,4 +35,22 @@ func main() {
         log.Fatal("ListenAndServe:", err)
     }
     NewOrg("Anonymous")
+}
+
+//-------------------------------------------------------
+// dummy users, to be removed
+//-------------------------------------------------------
+func createDummyUsers() {
+    org := &db.Organisation{ Name: "Test organisation" }
+    db.AddTemp("organisations", org)
+    db.AddTemp("users", &db.User{
+        OrgId: org.Id,
+        Email: "a@a.ua",
+        Password: "hardpass",
+    })
+    db.AddTemp("users", &db.User{
+        OrgId: org.Id,
+        Email: "b@a.ua",
+        Password: "hardpass",
+    })
 }
