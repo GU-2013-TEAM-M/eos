201085c2b10e42932ad156d44d7fd739c7a8de6c
Maksim Solovjov <himaxx2@gmail.com>
2014-02-05 23:56:51 +0000

    Make loginCheck tests JSON-based

--- server/server/login_check_handler_test.go
+++ server/server/login_check_handler_test.go
@@ -13,10 +13,14 @@ func Test_LoginCheckHandler(t *testing.T) {
     tmpU := &db.User{Id: tmp.UId, OrgId: bson.ObjectIdHex("52a4ed348350a921bd000002"), Email: "a", Password: "b"}
     db.AddTemp("users", tmpU)
 
-    lcmd := &CmdMessage{Data: make(map[string]interface{}), Conn: &Connection{owner: &User{}}}
-    lcmd.Data["session_id"] = tmp.Id.Hex()
+    msg := &Message{
+        msg: `{"type":"loginCheck","data":{
+            "session_id": "` + tmp.Id.Hex() + `"
+        }}`,
+        c: &Connection{ owner: &User{} },
+    }
 
-    LoginCheckHandler(lcmd)
+    err, _ := HandleMsg(msg)
 
     cmd := GetLastCmd()
     test.Assert(cmd.Data["status"].(string) == "OK", "it recognises the previous session", t)
@@ -24,15 +28,15 @@ func Test_LoginCheckHandler(t *testing.T) {
     db.DelTemps("sessions")
     db.DelTemps("users")
 
-    LoginCheckHandler(lcmd)
+    HandleMsg(msg)
 
     cmd = GetLastCmd()
     test.Assert(cmd.Data["status"].(string) == "UNAUTHORIZED", "it does not authorise user when there is no previous session", t)
 
-    lcmd.Data["session_id"] = "invalid"
-    err := LoginCheckHandler(lcmd)
+    msg.msg = `{"type":"loginCheck","data":{"session_id": "invalid"}}`
+    err, _ = HandleMsg(msg)
     test.Assert(err != nil, "It returns an error if session id is invalid objectid", t)
-    lcmd.Data["session_id"] = 5
-    err = LoginCheckHandler(lcmd)
+    msg.msg = `{"type":"loginCheck","data":{"session_id": 5}}`
+    err, _ = HandleMsg(msg)
     test.Assert(err != nil, "It returns an error if session id is invalid string", t)
 }
