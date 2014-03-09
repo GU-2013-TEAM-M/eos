ab58728cf1df8cd6117c495eb4ba1a452cfe6896
Maksim Solovjov <himaxx2@gmail.com>
2014-03-09 21:08:35 +0000

    Check if daemon supports the parameters passed to control

--- server/server/control_handler.go
+++ server/server/control_handler.go
@@ -23,8 +23,41 @@ func ControlHandler(cmd *CmdMessage) error {
         return errors.New("Trying to control inexistent daemon")
     }
 
+    // doing a pre-check, if the daemon supports the said parameters
+    operation, ok := cmd.Data["operation"].(map[string]interface{})
+    if !ok {
+        return errors.New("Format error")
+    }
+    start, startGiven := operation["start"].([]interface{})
+    stop, stopGiven := operation["stop"].([]interface{})
+    parameters := daemon.owner.(*Daemon).Entry.Parameters
+
+    if startGiven && !isSubset(start, parameters) ||
+        stopGiven && !isSubset(stop, parameters) {
+        return errors.New("Asking to start/stop unsupported parameter")
+    }
+
     data["status"] = "OK"
     DispatchMessage("control", data, cmd.Conn)
     DispatchMessage("control", cmd.Data, daemon)
     return nil
+}
+
+func isSubset(smaller []interface{}, larger []string) bool {
+    for _, el := range smaller {
+        str, ok := el.(string)
+        if !ok || !inArray(str, larger) {
+            return false;
+        }
+    }
+    return true;
+}
+
+func inArray(a string, arr []string) bool {
+    for _, el := range arr {
+        if a == el {
+            return true;
+        }
+    }
+    return false;
 }
--- server/server/control_handler_test.go
+++ server/server/control_handler_test.go
@@ -3,6 +3,7 @@ package main
 import (
     "testing"
     "eos/server/test"
+    "eos/server/db"
 )
 
 func Test_ControlHandler(t *testing.T) {
@@ -26,7 +27,11 @@ func Test_ControlHandler(t *testing.T) {
         Id: "a",
         OrgId: "Random_org",
         c: &Connection{},
+        Entry: &db.Daemon {
+            Parameters: []string { "cpu", "ram" },
+        },
     }
+    daemon.c.owner = daemon;
     daemon.Authorise()
 
     // the user has to be authorised
@@ -40,13 +45,28 @@ func Test_ControlHandler(t *testing.T) {
     err, _ = HandleMsg(msg)
     test.Assert(err != nil, "the daemon has to exist in the org", t)
 
-    // it transmutes the message if the daemon exists
+    // it transmutes the correct message if the daemon exists
     daemon.Deauthorise()
     daemon.OrgId = "Anonymous"
     daemon.Authorise()
 
     err, _ = HandleMsg(msg)
     test.Assert(err == nil, "it does not send the error, when all is ok", t)
+
+    // but it throws an error if we ask for unsupported parameters
+    msg.msg = `{
+        "type": "control",
+        "data": {
+            "daemon_id": "a",
+            "operation": {
+                "start": ["cpu"],
+                "stop": ["something"]
+            }
+        }
+    }`
+
+    err, _ = HandleMsg(msg)
+    test.Assert(err != nil, "it does not allow to ask for unsupported parameters", t)
 
     // it is only available to user
     msg.c = &Connection{owner: &Daemon{}}
